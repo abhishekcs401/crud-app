da55168c73ab0649d3dc6e693a74c2f1b4b6909d
abhishek <abhishek@thedreamsteps.com>
2025-03-07 14:32:55 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button, Container } from 'react-bootstrap';
+function App() {
+
+
+  return (
+    <>
+     <Container className="mt-5">
+      <h1>Welcome to React Bootstrap with Vite</h1>
+      <Button variant="primary">Click Me</Button>
+    </Container>
+    </>
+  )
+}
+
+export default App
